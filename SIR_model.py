df37493a6e7f812b18025d4347c4fb5885dfe879
ChrisW-priv <chris.watras@gmail.com>
2020-11-16 14:01:09 +0100

    All Basic functionality implemented - ready to start tinkering with

--- SIR_model.py
+++ SIR_model.py
@@ -1,10 +1,10 @@
 import random as rd
 
-class Agent():
-	def __init__(self, pos=(0,0), max_x_range=10, max_y_range=10, sick=False, max_x_range=1):
-		self.position = pos
-		self.max_x_range = max_x_range
-		self.max_y_range = max_y_range
+class Agent:
+	def __init__(self, pos=(0,0), b_shape=(10,10), sick=False, moving_range=1):
+		self.pos = pos
+		self.max_x_range = b_shape[0]
+		self.max_y_range = b_shape[1]
 		self.is_sick = sick
 		self.moving_range = moving_range
 		self.recovered = False
@@ -15,12 +15,12 @@ class Agent():
 			try:
 				x = rd.randrange(self.moving_range)
 				y = rd.randrange(self.moving_range)
-				assert self.position[0]+x <= self.max_x_range
-				assert self.position[1]+y <= self.max_y_range
+				assert self.pos[0]+x <= self.max_x_range
+				assert self.pos[1]+y <= self.max_y_range
 			except AssertionError:
 				pass
 			else:
-				self.position = ( self.position[0]+x, self.position[1]+y )
+				self.pos = ( self.pos[0]+x, self.pos[1]+y )
 				valid = True
 
 
@@ -36,25 +36,39 @@ class Controller:
 		self.disease_spread_distance = disease_spread_distance
 		self.beta=beta
 		self.gamma=gamma
+		self.time_steps = time_steps
 		
 		self.agents = []
-		self.crate_agents()
+
+	def start_sim(self):
 		with open('Disease_spread.csv', 'w') as new:
 			new.write('Time step,Susceptibles,Infected,Recovered\n')
-			new.write(f'0,{self.susceptibles},{sick_agents},0\n')
+			new.write(f'0,{self.susceptibles},{self.sick_agents},0\n')
 
-		for time_step in range(1, time_steps+1):
+		self.crate_agents()	
+
+		for time_step in range(1, self.time_steps+1):
 			self.time_step(time_step)
 
 	def crate_agents(self):
 		for _ in range(self.sick_agents):
-			pos = (rd.randrange(self.b_shape[0]), rd.randrange(self.b_shape[0]))
-			agent = Agent(pos, self.b_shape[0], self.b_shape[1], True)
+			agent_properties={
+				'pos':(rd.randrange(self.b_shape[0]), rd.randrange(self.b_shape[1])),
+				'b_shape':self.b_shape,
+				'sick':True,
+				'moving_range':1,
+			}
+			agent = Agent(**agent_properties)
 			self.agents.append(agent)
 
 		for _ in range(self.susceptibles):
-			pos = (rd.randrange(self.b_shape[0]), rd.randrange(self.b_shape[0]))
-			agent = Agent(pos, self.b_shape[0], self.b_shape[1])
+			agent_properties={
+				'pos':(rd.randrange(self.b_shape[0]), rd.randrange(self.b_shape[1])),
+				'b_shape':self.b_shape,
+				'sick':False,
+				'moving_range':1,
+			}
+			agent = Agent(**agent_properties)
 			self.agents.append(agent)
 
 	def agents_move(self):
@@ -63,19 +77,41 @@ class Controller:
 
 	def agents_spread_disease(self):
 		for agent in self.agents:
-			pass
+			if agent.is_sick:
+				cx,cy = agent.pos
+				for agent in self.agents:
+					x,y = agent.pos
+					if not agent.is_sick and not agent.recovered and ((cx-x)**2+(cy-y)**2)**0.5<=self.disease_spread_distance and rd.random()>self.beta:
+						agent.is_sick=True
+						self.susceptibles-=1
+						self.sick_agents+=1
 
 	def agents_recover(self):
 		for agent in self.agents:
 			if agent.is_sick and rd.random()>self.gamma:
 				agent.is_sick=False
-				agant.recovered=True
+				agent.recovered=True
+				self.sick_agents-=1
+				self.recovered+=1
 
 	def time_step(self, time_step):
 		self.agents_move()
 		self.agents_spread_disease()
 		self.agents_recover()
 
-		with open('Disease_spread.csv', 'a' ) as file:
+		with open('Disease_spread.csv', 'a') as file:
 			file.write(f'{time_step},{self.susceptibles},{self.sick_agents},{self.recovered}\n')
 
+
+if __name__ == "__main__":
+	sim_properties = {
+		'n_agents':10,
+		'sick_agents':1,
+		'b_shape':(10,10),
+		'beta': .6,
+		'gamma': .6,
+		'disease_spread_distance':1,
+		'time_steps':5
+	}
+	contoller = Controller(**sim_properties) 
+	contoller.start_sim()
